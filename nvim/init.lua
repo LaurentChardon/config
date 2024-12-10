a90325b4e4d615da109e345a102b31f0eb05c0c1
Laurent Chardon <laurent.chardon@gmail.com>
2024-12-10 11:18:44 -0500

    Use lazynvim for nvim configuration

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,2 @@
--- Neovim configuration file
-
--- Basic settings
--- vim.opt.number = true          -- Show line numbers
--- vim.opt.relativenumber = true  -- Show relative line numbers
-
--- Disable mouse support
-vim.opt.mouse = ""
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
