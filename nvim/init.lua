fe96eefc7ce82254cf516c04b8bb704906ccda31
Laurent Chardon <laurent.chardon@gmail.com>
2025-01-25 09:27:31 -0500

    nvim: add theme github

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,22 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Search Configuration
+vim.opt.ignorecase = false   -- Make search case-sensitive
+vim.opt.smartcase = false    -- Don't let nvim decide case sensitivity
+vim.opt.hlsearch = true      -- Highlight search matches
+vim.opt.incsearch = true     -- Show search matches as you type
+vim.opt.wrapscan = true      -- Wrap around the search
+
+-- Load github dark theme
+vim.cmd('colorscheme github_dark')
+-- Override the background color
+vim.cmd("highlight Normal guibg=#0d1117")
+
+-- Set colortheme
+-- vim.cmd("colorscheme tokyonight-night")
+--
+-- Use standard space per tab for display
+vim.opt.expandtab = false  -- Use tabs instead of spaces
+vim.opt.tabstop = 8        -- Number of spaces per tab (for display)
+vim.opt.shiftwidth = 8     -- Number of spaces for autoindent
