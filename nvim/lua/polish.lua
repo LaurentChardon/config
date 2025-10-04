2f6db23f32ae6c03dab51ded7b5467f23e49d733
Laurent Chardon <laurent.chardon@gmail.com>
2025-10-04 08:59:25 -0400

    nvim: improve makefile highlighting

--- nvim/lua/polish.lua
+++ nvim/lua/polish.lua
@@ -1,5 +1,25 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "make",
+  callback = function()
+    -- Load the habamax colorscheme
+    vim.cmd("colorscheme default")
+
+    -- Make everything transparent
+    local groups = {
+      "Normal", "NormalNC",
+      "SignColumn", "EndOfBuffer", "LineNr",
+      "StatusLine", "StatusLineNC",
+      "TabLine", "TabLineSel", "TabLineFill",
+      "WinBar", "WinBarNC",
+    }
+
+    for _, g in ipairs(groups) do
+      vim.cmd("hi! " .. g .. " guibg=NONE ctermbg=NONE")
+    end
+  end,
+})
+
