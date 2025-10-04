24b5c1d33ef9976ebbe961c9097e542a9a3e2f82
Laurent Chardon <laurent.chardon@gmail.com>
2025-10-04 08:26:46 -0400

    nvim: adjust colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -9,24 +9,31 @@ return {
 
         -- Enable transparent background
         transparent = true,
-
         -- Enable italic comment
         italic_comments = true,
-
         -- Enable italic inlay type hints
         italic_inlayhints = true,
-
         -- Underline `@markup.link.*` variants
         underline_links = true,
-
         -- Disable nvim-tree background color
         disable_nvimtree_bg = true,
-
         -- Apply theme colors to terminal
         terminal_colors = false,
 
       })
+
       require("vscode").load()
+
+      -- Override highlight groups for transparency in UI bars
+      vim.cmd([[
+        hi! StatusLine guibg=NONE ctermbg=NONE
+        hi! StatusLineNC guibg=NONE ctermbg=NONE
+        hi! TabLine guibg=NONE ctermbg=NONE
+        hi! TabLineFill guibg=NONE ctermbg=NONE
+        hi! WinBar guibg=NONE ctermbg=NONE
+        hi! WinBarNC guibg=NONE ctermbg=NONE
+      ]])
+
     end,
   },
 }
